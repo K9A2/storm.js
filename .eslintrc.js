126861783924e8a4a0e2183d3a04e5498005aee5
stormlin <lin-jinting@qq.com>
2018-12-20 19:37:46 +0800

    Remove rules on mandatory semi and double quote

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,15 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: "eslint:recommended",
+  extends: 'eslint:recommended',
   parserOptions: {
-    sourceType: "module"
+    sourceType: 'module'
   },
   rules: {
-    "no-console": "off",
-    indent: ["error", 4],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"]
+    'no-console': 'off',
+    indent: ['error', 4],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
   }
-};
+}

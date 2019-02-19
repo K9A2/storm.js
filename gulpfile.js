3c2708b3ff9bb61361763c9e04e1c28e90645155
stormlin <lin-jinting@qq.com>
2019-02-19 20:20:22 +0800

    升级依赖项

--- gulpfile.js
+++ gulpfile.js
@@ -1,119 +1,139 @@
 /* 引入与 gulp 处理相关的模块 */
-var gulp = require("gulp");
+var gulp = require('gulp')
 
-/* Less 文件的编译与处理 */
-var less = require("gulp-less");
-var cleanCSS = require("gulp-clean-css");
-var autoprefixer = require("gulp-autoprefixer");
+var cleanCSS = require('gulp-clean-css')
+var autoprefixer = require('gulp-autoprefixer')
 
 /* JavaScript 文件压缩 */
-var uglify = require("gulp-uglify");
+var uglify = require('gulp-uglify')
 
 /* 图像处理包 */
-var imagemin = require("gulp-imagemin");
-var pngquant = require("imagemin-pngquant");
-var cache = require("gulp-cache");
+var imagemin = require('gulp-imagemin')
+var pngquant = require('imagemin-pngquant')
+var cache = require('gulp-cache')
 
 /* 通用工具包 */
-var concat = require("gulp-concat");
-var rename = require("gulp-rename");
-var sourcemaps = require("gulp-sourcemaps");
-var plumber = require("gulp-plumber");
-var del = require("del");
-var fse = require("fs-extra");
+var concat = require('gulp-concat')
+var del = require('del')
 
 /* 主题设置 */
-var config = require("./src/config/config.json");
-var themePath = "./src/theme/" + config.theme;
+var config = require('./src/config/config.json')
+var themePath = './src/theme/' + config.theme
 
 /* 生成并输出 HTML 文件 */
-gulp.task("html", function () {
-    /* 引入与页面生成相关的模块 */
-    var themeConfig = require(themePath + "/themeConfig.json");
-    // 加载博客信息
-    var posts = require("./src/draft/description").posts;
-    var pages = require("./src/draft/description").pages;
-    var generator = require("./src/theme/" + config.theme + "/generate");
+gulp.task('html', done => {
+  /* 引入与页面生成相关的模块 */
+  var themeConfig = require(themePath + '/themeConfig.json')
+  // 加载博客信息
+  var posts = require('./src/draft/description').posts
+  var pages = require('./src/draft/description').pages
+  var generator = require('./src/theme/' + config.theme + '/generate')
 
-    generator.generate(posts, pages, config, themeConfig);
+  generator.generate(posts, pages, config, themeConfig)
 
-    /* 复制生成的 HTML 文件 */
-    gulp.src("./src/out/html/*.html")
-        .pipe(gulp.dest("./dist/"));
-});
+  /* 复制生成的 HTML 文件 */
+  gulp.src('./src/out/html/*.html').pipe(gulp.dest('./dist/'))
+
+  done()
+})
 
 /* CSS 压缩、合并、重命名、添加浏览器前缀、输出 */
-gulp.task("css", ["html"], function () {
-    gulp.src(themePath + "/css/" + "*.css")
-        .pipe(autoprefixer({
-            browsers: ["> 5%"],
-            cascade: false
-        }))
-        .pipe(concat("deploy.min.css"))
-        .pipe(cleanCSS({
-            compatibility: "ie8"
-        }))
-        .pipe(gulp.dest("./dist/css"));
-});
+gulp.task(
+  'css',
+  gulp.series('html', done => {
+    gulp
+      .src(themePath + '/css/' + '*.css')
+      .pipe(
+        autoprefixer({
+          browsers: ['> 5%'],
+          cascade: false
+        })
+      )
+      .pipe(concat('deploy.min.css'))
+      .pipe(
+        cleanCSS({
+          compatibility: 'ie8'
+        })
+      )
+      .pipe(gulp.dest('./dist/css'))
+    done()
+  })
+)
 
 /* JavaScript 文件的压缩、合并、输出 */
-gulp.task("js", function () {
+gulp.task('js', done => {
+  // 压缩主题自定义 js 文件
+  gulp
+    .src(themePath + '/js/custom.js')
+    .pipe(concat('custom.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
 
-    // 压缩主题自定义 js 文件
-    gulp.src(themePath + "/js/custom.js")
-        .pipe(concat("custom.min.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist/js"));
+  // 复制不需要压缩的库文件
+  gulp
+    .src('src/theme/' + config.theme + '/js/{highlight.min,jquery.min}.js')
+    .pipe(gulp.dest('./dist/js'))
 
-    // 复制不需要压缩的库文件
-    gulp.src("src/theme/" + config.theme + "/js/{highlight.min,jquery.min}.js")
-        .pipe(gulp.dest("./dist/js"));
+  done()
+})
 
-});
-
-/* 图片文件的压缩与输出 */
-gulp.task("img", ["html"], function () {
+gulp.task(
+  'img',
+  gulp.series('html', done => {
     // 复制文章自带的图片
-    gulp.src("./src/out/attachment/*.{png,jpg,gif}")
-        .pipe(cache(
-            imagemin({
-                progressive: true,
-                use: [pngquant()]
-            })
-        ))
-        .pipe(gulp.dest("./dist/attachment"));
+    gulp
+      .src('./src/out/attachment/*.{png,jpg,gif}')
+      // .pipe(
+      //   cache(
+      //     imagemin({
+      //       progressive: true,
+      //       use: [pngquant()]
+      //     })
+      //   )
+      // )
+      .pipe(gulp.dest('./dist/attachment'))
     // 复制主题自带的图片
-    gulp.src(themePath + "/img/*.{png,jpg,gif}")
-        .pipe(cache(
-            imagemin({
-                progressive: true,
-                use: [pngquant()]
-            })
-        ))
-        .pipe(gulp.dest("./dist/img"));
-});
+    gulp
+      .src(themePath + '/img/*.{png,jpg,gif}')
+      // .pipe(
+      //   cache(
+      //     imagemin({
+      //       progressive: true,
+      //       use: [pngquant()]
+      //     })
+      //   )
+      // )
+      .pipe(gulp.dest('./dist/img'))
+    done()
+  })
+)
 
 /**
  * 复制 description.json 至输出文件夹
  */
-gulp.task("description", function () {
-    gulp.src("./src/draft/description.json")
-        .pipe(gulp.dest("./dist"));
-});
+gulp.task('description', done => {
+  gulp.src('./src/draft/description.json').pipe(gulp.dest('./dist'))
+  done()
+})
 
-/* 清理输出文件夹 */
-gulp.task("cleanDistFolder", function () {
-    del.sync("./dist");
-});
+// /* 清理输出文件夹 */
+gulp.task('cleanDistFolder', done => {
+  del.sync('./dist')
+  done()
+})
 
 /* 程序执行后需要手动运行 orz */
-gulp.task("clean", function () {
-    console.log("in clean");
-    del.sync("./src/out");
-    del.sync("./dist");
-});
+gulp.task('clean', done => {
+  del.sync('./src/out')
+  del.sync('./dist')
+  done()
+})
 
 /* 总任务 */
-gulp.task("build", ["cleanDistFolder", "css", "js", "img", "description"], function () {
-    console.log("finish");
-});
+gulp.task(
+  'build',
+  gulp.series('cleanDistFolder', 'css', 'js', 'img', 'description', done => {
+    console.log('finish')
+    done()
+  })
+)
